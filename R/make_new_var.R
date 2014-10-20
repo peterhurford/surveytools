d40292672f32e41e00ec87b174ab048ab636bbe1
Peter Hurford <peter@peterhurford.com>
2014-10-20 00:22:49 -0500

    make_new_var

---
+++ R/make_new_var.R
@@ -0,0 +1,19 @@
+#' Given a melted data frame and a definition for the new variable, create
+#' that new variable added onto the melted data frame.
+#'
+#' @param new_var_name character.  The desired name of the new variable.
+#' @param definition numeric or character.  The values for the new variable.
+#'
+#' @param data.  The data to be transformed.  This is assumed to be a three
+#' column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
+#' The first column should contain IDs, the second column should contain variable
+#' names, and the third column should contain variable values.
+#' @export
+make_new_var <- function(new_var_name, definition, data) {
+  data <- rbind(data, data.frame(
+    Response.ID = unique(data[[1]]),
+    variable = new_var_name,
+    value = definition
+  ))
+  data
+}
--- R/swap_by_ids.R
+++ R/swap_by_ids.R
@@ -8,7 +8,7 @@
 #' @param variable character. The name of the variable where the replacement occurs.
 #'
 #' @param data.  The data to be transformed.  This is assumed to be a three
-#' column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>
+#' column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
 #' The first column should contain IDs, the second column should contain variable names,
 #' and the third column should contain variable values.
 #' @export
