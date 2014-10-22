201735c26eece808fa5fc3e2a0cc6a39ebbb2c05
Peter Hurford <peter@peterhurford.com>
2014-10-22 14:37:40 -0500

    fetch_var_by can now take a col argument

--- R/fetch_var_by.R
+++ R/fetch_var_by.R
@@ -1,8 +1,14 @@
 #' Fetches the values of one variable where a second variable is a certain value.
 #'
 #' @param first_var character. The name of the variable you want values returned for.
+#'
 #' @param by_var character. The name of the variable you want to sort by.
+#'
 #' @param select. The value by_var should be.
+#'
+#' @param col. 3 (default) to grap values, 2 to grab the variable name, 1 to grab
+#' the ids, or 'all' to grab the entire data frame for that variable.
+#'
 #' @param data. The data to grap values from. This is assumed to be a three column
 #' melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
 #' The first column should contain IDs, the second column should contain variable
@@ -11,7 +17,11 @@
 #' e.g., to return the favorite food of people from the city of Paris:
 #' \code{fetch_var_by(favorite_food, home_city, 'Paris', data)}
 #' @export
-fetch_var_by <- function(first_var, by_var, select, data) {
+fetch_var_by <- function(first_var, by_var, select, col = 3, data) {
   ids <- fetch_var(by_var, select = select, data = data, col = 1)
-  data[data[[1]] %in% ids & data[[2]] == first_var, 3]
+  if (!identical(col, 'all')) {
+    data[data[[1]] %in% ids & data[[2]] == first_var, col]
+  } else {
+    data[data[[1]] %in% ids & data[[2]] == first_var, ]
+  }
 }
