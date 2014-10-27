84bb11a6173c923e9ba789a72e8060de520328bd
Peter Hurford <peter@peterhurford.com>
2014-10-27 00:30:17 -0500

    fetch_var_by can remove NAs

--- R/fetch_var_by.R
+++ R/fetch_var_by.R
@@ -8,6 +8,8 @@
 #' @param col. 3 (default) to grap values, 2 to grab the variable name, 1 to grab
 #' the ids, or 'all' to grab the entire data frame for that variable.
 #'
+#' @param na.rm logical. Whether or not to remove NA and NA-like values.
+#'
 #' @param data. The data to grap values from. This is assumed to be a three column
 #' melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
 #' The first column should contain IDs, the second column should contain variable
@@ -16,17 +18,17 @@
 #' e.g., to return the favorite food of people from the city of Paris:
 #' \code{fetch_var_by(favorite_food, home_city, 'Paris', data)}
 #' @export
-fetch_var_by <- function(var, select_list, col = 3, data = get_data()) {
-  ids <- unique(data[[1]])
-  sapply(names(select_list), function(x) {
-    ids <<- base:::intersect(
-      ids,
-      fetch_var(x, select = select_list[[x]], data = data, col = 1)
-    )
-  })
-  if (!identical(col, 'all')) {
-    data[data[[1]] %in% ids & data[[2]] == var, col]
-  } else {
-    data[data[[1]] %in% ids & data[[2]] == var, ]
-  }
+fetch_var_by <- function(var, select_list, col = 3,
+  na.rm = TRUE, data = get_data()) {
+    ids <- unique(data[[1]])
+    sapply(names(select_list), function(x) {
+      ids <<- base:::intersect(
+        ids,
+        fetch_var(x, select = select_list[[x]],
+          na.rm = na.rm, data = data, col = 1)
+      )
+    })
+    data <- data[data[[1]] %in% ids & data[[2]] == var, ]
+    if (na.rm) data <- na.rm(data)
+    if (!identical(col, 'all')) data[[col]] else data
 }
