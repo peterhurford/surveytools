5ad3fcfbc0599c41aa4e7e686f0c8e496927f76c
Peter Hurford <peter@peterhurford.com>
2014-10-27 00:48:54 -0500

    na.rm and percent_table can take vectors or melted data frames

--- R/na.rm.R
+++ R/na.rm.R
@@ -1,9 +1,14 @@
 #' Deletes all NA and NA-like values from a melted data frame. 
-#' @param data.  The data to be transformed.  This is assumed to be a three
-#' column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
+#' @param data.  The data to be transformed.  This is assumed to either be a vector
+#' or a three column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
 #' The first column should contain IDs, the second column should contain variable
 #' names, and the third column should contain variable values.
 #' @export
 na.rm <- function(data) {
-  data <- data[!is.na(data[[3]]) & data[[3]] != "" & data[[3]] != "NA" & data[[3]] != "N/A" & data[[3]] != "", ]
+  if(identical(class(data), 'data.frame')) subdata <- data[[3]] else subdata <- data
+  subdata <- subdata[!is.na(subdata) & subdata != "" & subdata != "NA" & subdata != "N/A" & subdata != ""]
+  if(identical(class(data), 'data.frame')) {
+    data[[3]] <- subdata
+    data
+  } else { subdata }
 }                                                                                 
--- R/percent_table.R
+++ R/percent_table.R
@@ -1,14 +1,18 @@
 #' Prints a percent table.
 #' @param var characer. The name of the variable you want percent breakdown for.
-#' @param df data frame. The data frame of the variable to run the table for.  This dataframe is still assumed
-#' to be three column (id, variable name, value), but could be the result of a fetch_var() or fetch_var_by()
+#' @param data.  The data to be displayed.  This is assumed to either be a vector
+#' or a three column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
 #' @param round integer. How many digits you want to round percentages to. Default no rounding.
 #' @param na.rm logical. Remove all the NA, "N/A", or "" values. On by default.
 #' @export
-percent_table <- function(df, round = NULL, na.rm = TRUE) {
-  if (na.rm) na.rm(df)
-  sapply(unique(df[[3]]), function(x) {
-    percent <- length(df[df[[3]] == x,3]) / length(df[[3]]) * 100
+percent_table <- function(data, round = NULL, na.rm = TRUE) {
+  if (identical(class(data), 'data.frame')) data <- data[[3]]
+  if (na.rm) na.rm(data)
+  sapply(unique(data), function(x) {
+    percent <- if (identical(class(data), 'data.frame'))
+      length(data[data == x,3]) / length(data) * 100
+    else
+      length(data[data == x]) / length(data) * 100
     if (!is.null(round)) percent <- round(percent, digits = round)
     print(paste0(x, ": ", percent, "%"))
   })
