3f2797754e13ea69ac361cef3a363fd1ffef4be6
Peter Hurford <peter@peterhurford.com>
2014-10-27 00:56:56 -0500

    ids(data) and varnames(data)

--- R/varnames.R
+++ R/varnames.R
@@ -1,7 +1,7 @@
-#' Sets the data parameter to a certain data frame.
+#' Gets the variable names of the melted data frame.
 #' @param data.  The data to be transformed.  This is assumed to be a three
 #' column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
 #' The first column should contain IDs, the second column should contain variable
 #' names, and the third column should contain variable values.
 #' @export
-set_data <- function(data) { surveytools_data <<- data }
+varnames <- function(data) { unique(data[[2]]) }
