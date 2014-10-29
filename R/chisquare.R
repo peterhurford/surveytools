b4a4137c13beaaa6b9ee63e99765284d4bd3a257
Peter Hurford <peter@peterhurford.com>
2014-10-28 22:16:46 -0500

    document for chisquare, cleaning

--- R/chisquare.R
+++ R/chisquare.R
@@ -1,3 +1,20 @@
+#' Runs a chi square test on data within a melted dataframe.
+#'
+#' Gracefully handles differences in variable size and NAs, and allows easy
+#' removal of variables.
+#'
+#' @param var character. The name of the first variable in the chi-square test.
+#' @param by_var charager. The name of the second (by) variable in the
+#'     chi-square test.
+#' @param ignore vector. A vector of the names of values to be replaced by NAs
+#'     within both the var and the by_var. Can clean up categories that are too
+#'     small for meaningful analysis.
+#'
+#' @param data.  The data to be transformed.  This is assumed to be a three
+#' column melted dataframe, using reshape2 <http://cran.r-project.org/web/packages/reshape2/index.html>.
+#' The first column should contain IDs, the second column should contain variable
+#' names, and the third column should contain variable values.
+#' @export
 chisquare <- function(var, by_var, ignore = NULL, data = get_data()) {
   x <- fetch_var(var, col = 'all')
   y <- fetch_var(by_var, col = 'all')
@@ -11,6 +28,5 @@ chisquare <- function(var, by_var, ignore = NULL, data = get_data()) {
   y <- na.rm(y)
   x2 <- x[x[[1]] %in% y[[1]], 3]
   y2 <- y[y[[1]] %in% x[[1]], 3]
-  browser()
   chisq.test(table(x2, y2))
 }
